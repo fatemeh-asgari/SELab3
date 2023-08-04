789bcf18ecb887a922a7033d6cb7b0202b9449dd
aminaryan80 <aryanmohammadamin@gmail.com>
2023-08-04 20:28:38 +0330

    Implement power feature to pass tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -14,4 +14,15 @@ public class Calculator {
         return a / b;
     }
 
+    public int power(int a, int b) {
+        if (b < 0)
+            throw new IllegalArgumentException();
+
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    }
+
 }
